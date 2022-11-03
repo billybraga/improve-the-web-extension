9431e968a98ee36e1da2f9035da1c9b9353d31a8
Billy <billy.braga@gmail.com>
2022-11-02 20:29:44 -0400

    notifs and play_pause command

--- js/background.js
+++ js/background.js
@@ -10,28 +10,81 @@ function loadYtm() {
 function listen() {
     if (!window.__ytmMessageListenSet) {
         window.__ytmMessageListenSet = true;
-        console.info("Listening messages in tab within bg context");
+
+        window.addEventListener("message", (event) => {
+            if (event.data.destination === "extension") {
+                chrome.runtime.sendMessage(event.data);
+            }
+        }, false);
+
         chrome.runtime.onMessage.addListener(function (message) {
-            console.info("Received message in tab within bg context");
-            window.postMessage({ type: "volume_change", message: message }, "*");
+            console.info("Received message in tab within bg context", message);
+            if (message.destination === "content") {
+                window.postMessage(message, "*");
+            }
         });
     }
 }
 
+const notifTimeMs = 2000;
+let notifTimeoutIds = {};
+chrome.runtime.onMessage.addListener(function (message) {
+    if (message.type === "notif") {
+        const notifId = "__yt_" + message.notifType;
+        if (notifTimeoutIds[notifId]) {
+            console.log("Update notif", message.notifMessage);
+            clearTimeout(notifTimeoutIds[notifId]);
+            chrome.notifications.update(
+                notifId,
+                {
+                    message: message.notifMessage
+                }
+            );
+        } else {
+            console.log("Create notif", message.notifMessage);
+            chrome.notifications.create(
+                notifId,
+                {
+                    type: "basic",
+                    silent: true,
+                    iconUrl: "/img/favicon_144.png",
+                    title: message.notifTitle,
+                    message: message.notifMessage
+                }
+            );
+        }
+
+        notifTimeoutIds[notifId] = setTimeout(() => {
+            notifTimeoutIds[notifId] = null;
+            console.log("Clear notif");
+            chrome.notifications.clear(notifId, x => console.log("Clear callback", x));
+        }, notifTimeMs);
+    }
+});
+
 chrome.commands.onCommand.addListener(function (command) {
-    const direction = command.split('-')[1];
+    const parts = command.split('-');
+    const type = parts[0];
+    const arg = parts[1];
 
     console.info("Received command in bg", command);
+    const message = { type: type, arg, destination: "content" };
     chrome.tabs.query({ url: "https://music.youtube.com/*" }, function (tabs) {
         tabs.forEach(tab => {
-            console.info("Sending command in to tab", tab.id, command);
-            chrome.tabs.sendMessage(tab.id, { direction });
+            console.info("Sending command in to tab", tab.id, message);
+            chrome.tabs.sendMessage(tab.id, message);
+        });
+    });
+    chrome.tabs.query({ url: "https://youtube.com/*" }, function (tabs) {
+        tabs.forEach(tab => {
+            console.info("Sending command in to tab", tab.id, message);
+            chrome.tabs.sendMessage(tab.id, message);
         });
     });
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (tab.url.indexOf("https://music.youtube.com") === 0 && changeInfo.status == 'complete' && tab.active) {
+    if (tab.url.indexOf("https://music.youtube.com") === 0 && changeInfo.status === 'complete' && tab.active) {
         chrome.scripting.executeScript({
             target: {tabId},
             func: loadYtm
--- js/ytm.js
+++ js/ytm.js
@@ -37,9 +37,9 @@ if (!window.__ytmLoaded) {
         // 30 steps
         const deltaTimeForChange = fadeTime / steps;
         console.info("Will set volume to " + dest + " in " + steps + " " + deltaTimeForChange + "ms / " + delta + "% steps");
-        
+
         innerFade();
-        
+
         function innerFade() {
             let tagVol = videoTag.volume * 100;
             if ((dir === -1 && tagVol <= dest) || (dir === 1 && tagVol >= dest)) {
@@ -80,7 +80,8 @@ if (!window.__ytmLoaded) {
                 fade(
                     1,
                     targetVolume,
-                    () => {}
+                    () => {
+                    }
                 );
             }
         },
@@ -88,25 +89,58 @@ if (!window.__ytmLoaded) {
     );
 
     window.addEventListener("message", (event) => {
+        console.info("got message in ytm", event);
         // We only accept messages from ourselves
-        if (event.source !== window) {
-          return;
+        if (event.source !== window || event.data.destination !== 'content') {
+            return;
         }
-      
-        if (event.data.type && (event.data.type === "volume_change")) {
+
+        if (event.data.type) {
             console.log("Content script received", event.data);
-            const { direction } = event.data.message;
-            handleVolumeCommand(direction);
+            let notifTitle;
+            let notifMessage;
+            if (event.data.type === "volume_change") {
+                const newVol = handleVolumeCommand(event.data.arg);
+
+                notifTitle = "YTM volume change";
+                notifMessage = newVol.toFixed(0) + "%";
+            } else if (event.data.type === "play_pause") {
+                playBtn.click();
+                notifTitle = "YTM state change";
+                // 1 is play, 2 is pause
+                if (playerApi.getPlayerState() === 1) {
+                    notifMessage = "Pause";
+                } else {
+                    notifMessage = "Play";
+                }
+            }
+            window.postMessage(
+                {
+                    type: "notif",
+                    notifType: event.data.type,
+                    notifMessage,
+                    notifTitle,
+                    destination: "extension"
+                });
         }
-      }, false);
+    }, false);
 
     function handleVolumeCommand(direction) {
         let volume = playerApi.getVolume();
         const unit = direction === 'down' ? -1 : 1;
-        const newVol = Math.min(100, Math.max(0, volume + (unit * volDelta)));
+        const relativeChange = unit * volDelta * (1 + (4 * volume / 100.0));
+        console.info("will change volume of", relativeChange);
+        const newVol = Math.min(
+            100,
+            Math.max(
+                0,
+                volume + relativeChange
+            )
+        );
         setApiVolume(newVol);
         localStorage["__ytmVol"] = newVol.toString();
+        return newVol;
     }
-    
+
     console.info("Loaded shortcuts");
 }
