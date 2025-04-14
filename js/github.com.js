49c2c115fbdd9878e4066194a64add3bc676b95a
Billy Braga <billy.braga@gmail.com>
2025-04-14 11:46:47 -0400

    update

--- js/github.com.js
+++ js/github.com.js
@@ -1,17 +1,26 @@
 if (!window.__itwLoaded) {
     window.__itwLoaded = true;
     console.log("Loaded github ext");
-    const sidePane = document.getElementById('repos-file-tree')?.parentElement?.parentElement;
-    if (sidePane) {
-        sidePane.style.cssText = '--pane-width:1000px';
-        sidePane.classList.add('ext-loaded');
-    }
+    // updateSidePane();
     
     document.addEventListener('DOMContentLoaded', function() {
+        // updateSidePane();
+        updateLogs();
+    });
+    
+    function updateSidePane() {
+        const sidePane = document.getElementById('repos-file-tree')?.parentElement?.parentElement;
+        if (sidePane) {
+            sidePane.style.cssText = '--pane-width:1000px';
+            sidePane.classList.add('ext-loaded');
+        }
+    }
+    
+    function updateLogs() {
         const element = document.querySelector('#logs');
         if (element) {
             element.setAttribute('data-color-mode', 'light');
             element.setAttribute('data-dark-theme', 'light');
         }
-    });
+    }
 }
